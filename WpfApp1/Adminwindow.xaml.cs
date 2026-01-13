e77a9de4badfc1bdcbdc47b424dcb7a932fc0b40
c3developer <nikitosina2388@gmail.com>
2026-01-13 17:35:36 +0300

    Window create

--- WpfApp1/Adminwindow.xaml.cs
+++ WpfApp1/Adminwindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Adminwindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Adminwindow : Window
     {
-        public MainWindow()
+        public Adminwindow()
         {
             InitializeComponent();
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-           
-            //if 
         }
     }
 }
